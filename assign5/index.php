49f4af3aa71b0acf30545a71201598cc0ebea69a
Andrew Wood <wood.andrew9@gmail.com>
2018-06-08 11:21:59 -0400

    First Commit

---
+++ assign5/index.php
@@ -0,0 +1,49 @@
+<!---
+Andrew Wood's ePortfolio
+COP4813 - Summer 2018
+index.html - PHP Introduction - Assignment 5
+--->
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>CSS Animations</title>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" /> 
+                <link rel="stylesheet" type="text/css" href="../style.css"></link>	
+        </head>
+    
+    	<body class="clearfix">
+		
+		<h1 style="text-align:center">Canvas Animation Magic</h1>
+		<p style="text-align:center">Let's have some fun with Canvas Animations!</p>
+		<hr />
+                <p>Content goes here</p>
+                <p>Other content goes here</p>
+                
+                <?php
+                    $my_string = "Hello Bob.  My name is: ";
+                    $my_number = 4;
+                    $my_letter = 'a';
+                    echo $my_string;
+                    echo $my_number;
+                    echo $my_letter;
+                ?>
+
+                
+                
+                <hr />
+		<h2>My ePortfolio Index</h2>
+		<p><a href="../index.html">Andrew's ePortfolio Index</a></p>
+	</body>
+</html>
+
+
+
+
+
+
+
+
